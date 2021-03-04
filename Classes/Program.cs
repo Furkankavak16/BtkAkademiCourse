2d1a0277ff8c2b440b9b05bd34092163dce6e247
Furkan Kavak <furkankvk1621@gmail.com>
2021-03-05 02:27:55 +0300

    04.03.2021

---
+++ Classes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            TeachingStaff teachingStaff = new TeachingStaff()
+            {
+                RegistrationNumber = 1,
+                FirstName = "Furkan",
+                LastName = "Kavak",
+                Gender = true
+            };
+            Console.WriteLine(teachingStaff);
+
+
+            var list = new List<TeachingStaff>()
+            {
+                new TeachingStaff{RegistrationNumber=1,FirstName="Salih",LastName="Bağlamış",Gender=true},
+                new TeachingStaff{RegistrationNumber=2,FirstName="Melih",LastName="Kaçmaz",Gender=true},
+                new TeachingStaff{RegistrationNumber=3,FirstName="Osman",LastName="Kavak",Gender=true},
+                new TeachingStaff{RegistrationNumber=4,FirstName="Berat",LastName="Orun",Gender=true},
+                new TeachingStaff{RegistrationNumber=5,FirstName="Zeynep",LastName="Özcan",Gender=false},
+                new TeachingStaff{RegistrationNumber=6,FirstName="Nergiz",LastName="Çiçek",Gender=false},
+
+
+
+            };
+            Console.WriteLine("List1 data");
+            list.ForEach(teach => Console.WriteLine(teach));
+            Console.WriteLine();
+
+          
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Classes/TeachingStaff.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public class TeachingStaff
+    {
+        public int RegistrationNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool Gender { get; set; }
+      
+
+        public override string ToString()
+        {
+            return $" {RegistrationNumber,5} " +
+                $" {FirstName,5} " +
+                $" {LastName,5} " +
+                string.Format("{0,5}", Gender == true ? "Erkek" : "Kadın");
+                    
+        }
+    }
+}
